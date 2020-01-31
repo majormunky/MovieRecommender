104d08c846720cbf06f742d51f64b3ea0984b313
Josh Bright <josh.bright@gmail.com>
2020-01-30 18:20:21 -0800

    Basic setup of models in the home app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class StreamingService(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    service = models.ForeignKey(
+        StreamingService, on_delete=models.PROTECT, blank=True, null=True
+    )
+    reviewed_by = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, through="MovieReview"
+    )
+
+
+class MovieReview(models.Model):
+    rating = models.IntegerField()
+    comment = models.TextField()
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
