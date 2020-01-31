a026c29ef1591dfad3caf4d1f2eb5626fefe3672
Josh Bright <josh.bright@gmail.com>
2020-01-30 20:02:05 -0800

    Added television shows to the models

--- home/models.py
+++ home/models.py
@@ -9,6 +9,17 @@ class StreamingService(models.Model):
         return self.name
 
 
+class TelevisionShow(models.Model):
+    title = models.CharField(max_length=64, unique=True)
+    service = models.ForeignKey(
+        StreamingService, on_delete=models.PROTECT, blank=True, null=True
+    )
+    reviewed_by = models.ManyToManyField(settings.AUTH_USER_MODEL, through="ShowReview")
+
+    def __str__(self):
+        return self.title
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=255, unique=True)
     service = models.ForeignKey(
@@ -26,4 +37,11 @@ class MovieReview(models.Model):
     rating = models.IntegerField()
     comment = models.TextField()
     movie = models.ForeignKey(Movie, on_delete=models.PROTECT)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+
+
+class ShowReview(models.Model):
+    rating = models.IntegerField()
+    comment = models.TextField()
+    show = models.ForeignKey(TelevisionShow, on_delete=models.PROTECT)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
