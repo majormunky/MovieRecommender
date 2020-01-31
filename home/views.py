69b3e79b79047e78e19bb80a1d04146b93f58e3c
Josh Bright <josh.bright@gmail.com>
2020-01-30 19:59:04 -0800

    Index page will now list the movies in the system

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from home import models
 
 
 @login_required
 def index(request):
-    return render(request, "home/index.html", {})
+    movie_list = models.Movie.objects.all()
+    streaming_list = models.StreamingService.objects.all()
+    return render(
+        request,
+        "home/index.html",
+        {"movie_list": movie_list, "streaming_list": streaming_list},
+    )
