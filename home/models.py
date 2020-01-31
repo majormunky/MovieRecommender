c4bd895fd31cb57b3be5cb30cac0599546e94958
Josh Bright <josh.bright@gmail.com>
2020-01-30 18:23:54 -0800

    Added str methods to the models

--- home/models.py
+++ home/models.py
@@ -5,6 +5,9 @@ from django.conf import settings
 class StreamingService(models.Model):
     name = models.CharField(max_length=64, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255, unique=True)
@@ -14,6 +17,9 @@ class Movie(models.Model):
     reviewed_by = models.ManyToManyField(
         settings.AUTH_USER_MODEL, through="MovieReview"
     )
+
+    def __str__(self):
+        return self.title
 
 
 class MovieReview(models.Model):
