eccf334bff7b9f5a65e58dc22783471b41152245
Josh Bright <josh.bright@gmail.com>
2020-01-30 19:45:23 -0800

    Added a profile page

--- movie_project/urls.py
+++ movie_project/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path(
+        "accounts/profile/",
+        TemplateView.as_view(template_name="profile.html"),
+        name="user_profile",
+    ),
     path("", include("home.urls")),
 ]
