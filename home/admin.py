c60bf3edc9aa99de65624c9e94633c234d49eee7
Josh Bright <josh.bright@gmail.com>
2020-01-30 18:21:59 -0800

    Added models to the admin

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from home import models
+
 
 # Register your models here.
+admin.site.register(models.Movie)
+admin.site.register(models.MovieReview)
+admin.site.register(models.StreamingService)
